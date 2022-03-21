5f7ff0e39c0c5fd394c300129541d1c45f4a1d44
hsorby <h.sorby@auckland.ac.nz>
2022-03-21 21:43:17 +1300

    Correct location of codebase.

--- mapclientplugins/convertdatatozincstep/__init__.py
+++ mapclientplugins/convertdatatozincstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin - Generated from MAP Client v0.17.0
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Convert Data to Zinc'
-__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.convertdatatozinc'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.convertdatatozincstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.convertdatatozincstep import step
